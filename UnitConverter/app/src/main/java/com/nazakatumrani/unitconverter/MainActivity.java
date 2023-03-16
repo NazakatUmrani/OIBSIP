be5c5307c139234c443d31b523b8d104e4f72408
NazakatUmrani <unazakat70@gmail.com>
2023-03-17 00:21:22 +0500

    Unit Converter App bug fixed, Meters to centimeters working

--- UnitConverter/app/src/main/java/com/nazakatumrani/unitconverter/MainActivity.java
+++ UnitConverter/app/src/main/java/com/nazakatumrani/unitconverter/MainActivity.java
@@ -16,20 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         button = findViewById(R.id.convertButton);
         textView = findViewById(R.id.resultText);
         editText = findViewById(R.id.inputField);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Toast.makeText(this, "Good Morning", Toast.LENGTH_SHORT).show();
+        textView.setText("Assalam u alaikum");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "this", Toast.LENGTH_SHORT).show();
+                textView.setText(Integer.toString(100 * Integer.parseInt(editText.getText().toString())));
             }
         });
-        //void convert(View view){
-        //    //textView.setText(String.format(Integer.toString(100 * Integer.parseInt(editText.getText().toString()))));
-        //}
     }
 }
